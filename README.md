1e2680754c4f753a37d0ed6fdc5bdc1c63616989
Danys24 <danisserna99@gmail.com>
2023-06-05 14:31:22 -0500

    website type landing page

---
+++ README.md
@@ -0,0 +1,8 @@
+# Personal Web Site
+project developed for the creation of my personal website. Made with the following technologies:
+
+* HTML
+* CSS
+* JavaScript
+
+link: https://danys24.github.io/website-portafolio
---
+++ script.js
@@ -0,0 +1,49 @@
+
+((d) => {
+  const $btnMenu = d.querySelector(".btn-menu"),
+   $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("display");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("display");
+  });
+})(document);
+
+
+/*formulario*/
+((d) => {
+  const $form = d.querySelector(".form-contact"),
+    $response = d.querySelector(".input-btn");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch("https://formsubmit.co/ajax/danisserna99@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+      });
+  });
+})(document);
